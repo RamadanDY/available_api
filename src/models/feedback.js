0d3be9a5ab92869466a0446360f485735ba7a4a6
DStudentDeveloper <int22011532@central.edu.gh>
2025-01-03 11:32:18 +0000

    ✨ Feat: Add timestamps to the `Feedback` model

--- src/models/feedback.js
+++ src/models/feedback.js
@@ -1,12 +1,15 @@
 import { Schema, model } from 'mongoose';
 
-const feedbackSchema = new Schema({
-  message: { type: String, required: true },
-  representativeId: String,
-  representativeName: String,
-  representativeCourse: String,
-  representativeLevel: String,
-});
+const feedbackSchema = new Schema(
+  {
+    message: { type: String, required: true },
+    representativeId: String,
+    representativeName: String,
+    representativeCourse: String,
+    representativeLevel: String,
+  },
+  { timestamps: true }
+);
 
 feedbackSchema.set('toJSON', { virtuals: true });
 feedbackSchema.set('toObject', { virtuals: true });
