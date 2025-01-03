6ba0c70e74d4f33bb3a6eff65546193812dbab03
DStudentDeveloper <int22011532@central.edu.gh>
2025-01-03 11:30:04 +0000

    ✨ Feat: Create a `Feedback` model

---
+++ src/models/feedback.js
@@ -0,0 +1,16 @@
+import { Schema, model } from 'mongoose';
+
+const feedbackSchema = new Schema({
+  message: { type: String, required: true },
+  representativeId: String,
+  representativeName: String,
+  representativeCourse: String,
+  representativeLevel: String,
+});
+
+feedbackSchema.set('toJSON', { virtuals: true });
+feedbackSchema.set('toObject', { virtuals: true });
+
+const Feedback = model('Feedback', feedbackSchema);
+
+export default Feedback;
