69a47c33ed1826cdc3fdcb1e362b3b6f392199dc
RamDev <daudalaramadan2@gmail.com>
2025-01-12 08:47:55 +0000

    refactor: update vercel configuration and remove unused response in getBlocks

--- src/controllers/blocks.js
+++ src/controllers/blocks.js
@@ -20,7 +20,6 @@ export async function createBlock(req, res, next) {
 
 export async function getBlocks(req, res, next) {
   try {
-    res.send('Hello from getBlocks');
     const blocks = await Block.find()
       .populate('classes', 'fullCode code isAvailable')
       .sort({ code: 1 });
